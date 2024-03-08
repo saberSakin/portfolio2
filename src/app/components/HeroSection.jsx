233b0ba963639daca5587a620cfd56df75c63c22
Mohammad Shakin <md.shakin18@gmail.com>
2024-03-09 02:06:46 +0600

    updates

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -45,7 +45,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="/"
+              href="https://drive.google.com/drive/folders/1GS9nS6_XqZxYiOgrSFf148_aTWywo9Iu?usp=sharing"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
